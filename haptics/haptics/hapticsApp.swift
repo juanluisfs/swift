723c7906b7fdfa5068e457d4c6cd6632d57233ff
Juan Luis Flores <juluflosa01.demos@gmail.com>
2024-07-03 10:38:07 -0600

    Add files via upload

---
+++ haptics/haptics/hapticsApp.swift
@@ -0,0 +1,17 @@
+//
+//  hapticsApp.swift
+//  haptics
+//
+//  Created by Juan Luis on 04/08/22.
+//
+
+import SwiftUI
+
+@main
+struct hapticsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
