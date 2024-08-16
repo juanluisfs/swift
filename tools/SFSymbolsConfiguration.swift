9f24646ba9c9198163c79b2056331d7fa2e0624f
Juan Luis Flores <juluflosa01.demos@gmail.com>
2024-08-16 00:25:42 -0600

    Create SFSymbolsConfiguration.swift

---
+++ tools/SFSymbolsConfiguration.swift
@@ -0,0 +1,3 @@
+// This is a summary of SF Symbols: https://developer.apple.com/design/human-interface-guidelines/sf-symbols/
+// Este es un resumen de SF Symbols: https://developer.apple.com/design/human-interface-guidelines/sf-symbols/
+
