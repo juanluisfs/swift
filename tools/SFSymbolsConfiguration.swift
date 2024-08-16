130cee1a4a14132d427bc610da511a92c765c493
Juan Luis Flores <juluflosa01.demos@gmail.com>
2024-08-16 14:28:43 -0600

    Update SFSymbolsConfiguration.swift

--- tools/SFSymbolsConfiguration.swift
+++ tools/SFSymbolsConfiguration.swift
@@ -1,3 +1,5 @@
 // This is a summary of SF Symbols: https://developer.apple.com/design/human-interface-guidelines/sf-symbols/
 // Este es un resumen de SF Symbols: https://developer.apple.com/design/human-interface-guidelines/sf-symbols/
 
+// Rendering Mode
+.renderingMode()
