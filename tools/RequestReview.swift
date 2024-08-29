2d96ca12ac0d58e69a63babcb14ad3ed4a6fa77a
Juan Luis Flores <juluflosa01.demos@gmail.com>
2024-08-29 02:20:31 -0600

    Create RequestReview.swift

---
+++ tools/RequestReview.swift
@@ -0,0 +1,12 @@
+import StoreKit
+import SWiftUI
+
+struct ContentView: View {
+
+  @Environment(\.requestReview) var requestReview
+
+  @State private var counter: Int = 0
+  
+  var body: some View {
+  }
+}
