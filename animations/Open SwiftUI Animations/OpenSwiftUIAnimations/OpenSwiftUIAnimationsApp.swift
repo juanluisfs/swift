e05105ec6ee24dace6b04b0d439f1c75bceb3755
Juan Luis Flores <juluflosa01.demos@gmail.com>
2024-09-10 14:20:12 -0600

    Add files via upload

---
+++ animations/Open SwiftUI Animations/OpenSwiftUIAnimations/OpenSwiftUIAnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenSwiftUIAnimationsApp.swift
+//  OpenSwiftUIAnimations
+//
+
+import SwiftUI
+
+@main
+struct OpenSwiftUIAnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            //StackedSpring()
+            //EmitterParticles()
+            WWDC24View1()
+        }
+    }
+}
