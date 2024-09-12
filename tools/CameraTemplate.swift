7916fa85ca35146efdc6b201026b037e74b39422
Juan Luis Flores <juluflosa01.demos@gmail.com>
2024-09-12 13:57:27 -0600

    Create CameraTemplate.swift

---
+++ tools/CameraTemplate.swift
@@ -0,0 +1,75 @@
+//
+//  ContentView.swift
+//  CameraTemplate
+//
+//  Created by Juan Luis on 10/08/24.
+//
+
+import SwiftUI
+import PhotosUI
+
+struct ContentView: View {
+    
+    @State private var showCamera = false
+    @State private var selectedImage: UIImage?
+    
+    var body: some View {
+        VStack {
+            if let selectedImage{
+                Image(uiImage: selectedImage)
+                    .resizable()
+                    .scaledToFit()
+            }
+            
+            Button("Open camera") {
+                self.showCamera.toggle()
+            }
+        }
+        .fullScreenCover(isPresented: self.$showCamera) {
+            accessCameraView(selectedImage: self.$selectedImage)
+                .ignoresSafeArea()
+        }
+    }
+}
+
+
+struct accessCameraView: UIViewControllerRepresentable {
+    
+    @Binding var selectedImage: UIImage?
+    @Environment(\.presentationMode) var isPresented
+    
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        let imagePicker = UIImagePickerController()
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false
+        imagePicker.delegate = context.coordinator
+        return imagePicker
+    }
+    
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {
+        
+    }
+
+    func makeCoordinator() -> Coordinator {
+        return Coordinator(picker: self)
+    }
+}
+
+// Coordinator will help to preview the selected image in the View.
+class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    var picker: accessCameraView
+    
+    init(picker: accessCameraView) {
+        self.picker = picker
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let selectedImage = info[.originalImage] as? UIImage else { return }
+        self.picker.selectedImage = selectedImage
+        self.picker.isPresented.wrappedValue.dismiss()
+    }
+}
+
+#Preview {
+    ContentView()
+}
