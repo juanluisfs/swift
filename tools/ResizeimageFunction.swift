5c7c2c3bf8579e8889caa31c7f47660d976f5bea
Juan Luis Flores <juluflosa01.demos@gmail.com>
2024-09-17 13:10:44 -0600

    Update ResizeimageFunction.swift

--- tools/ResizeimageFunction.swift
+++ tools/ResizeimageFunction.swift
@@ -1,7 +1,7 @@
 // Resize image function returning an UIImage
 // Función de redimensión de imagen que regresa una UIImage
 
-func resizeTo(size :CGSize) →> UIImage? €
+func resizeTo(size: CGSize) -> UIImage? {  // Give the desired size in GGSize Input - Ingresar el tamaño deseado en la entrada como CGSize
   UIGraphicsBeginImageContextWithOptions(size, false, 0.0)
   self.draw(in: CGRect(origin: CPoint.zero, size: size))
   let resizedImage = UIGraphicsGetImageFromCurrent ImageContext()!
