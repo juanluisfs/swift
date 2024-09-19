7759c15b4d4b0e5b3e87c0da0581b1719d311048
Juan Luis Flores <juluflosa01.demos@gmail.com>
2024-09-18 19:26:50 -0600

    Update CustomTextField.swift

--- ui/CustomTextField.swift
+++ ui/CustomTextField.swift
@@ -1,40 +1,54 @@
 import SwiftUI
 
-struct CustomTextField: View {
-    
+struct KardTextField: View {
+    // Local variables
     @Environment(\.colorScheme) var theme
     @FocusState var isTyping: Bool
-    @State var text = ""
-    @State var back = Color.white
+    @State var isShowing: Bool = false
+    @State var accentColor = Color.black
+    // Input variables
+    let title: String
+    @Binding var text: String
     
     var body: some View {
-        VStack {
-            ZStack(alignment: .leading) {
-                TextField("", text: $text)
-                    .padding(.leading)
-                    .frame(height: 55)
-                    .focused($isTyping)
-                    .background(isTyping ? .blue : Color.primary, in: RoundedRectangle(cornerRadius: 14).stroke(lineWidth: 2))
-                
-                Text("First Name")
-                    .padding(.horizontal, 5)
-                    .background(back.opacity(isTyping || !text.isEmpty ? 1 : 0))
-                    .foregroundStyle(isTyping || !text.isEmpty ? .blue : Color.primary)
-                    .padding(.leading)
-                    .offset(y: isTyping ? -27 : 0)
-                    .onTapGesture {
-                        isTyping.toggle()
+        ZStack(alignment: .leading) {
+            TextField("", text: $text)
+                .padding(.leading)
+                .frame(height: 48)
+                .focused($isTyping)
+                .background(isTyping ? .blue : Color.primary, in: RoundedRectangle(cornerRadius: 14).stroke(lineWidth: 2))
+            
+            Text(title)
+                .padding(.horizontal, 5)
+                .background(accentColor)
+                .foregroundStyle(isTyping ? .blue : Color.primary)
+                .padding(.leading)
+                .offset(y: isShowing ? -24 : 0)
+                .onChange(of: text, {   // Delete this onChange if not autoupdate is required
+                    if !isTyping && text.isEmpty {
+                        isShowing = false
+                    } else {
+                        isShowing = true
                     }
-                    .onChange(of: theme, {
-                        back = (theme == .dark ? .black : .white)
-                    })
-                    .animation(.linear(duration: 0.2), value: isTyping)
-            }
+                })
+                .onChange(of: isTyping, {
+                    if !isTyping && text.isEmpty {
+                        isShowing = false
+                    } else {
+                        isShowing = true
+                    }
+                })
+                .onChange(of: theme, {
+                    accentColor = (theme == .dark ? .black : .white)
+                })
+                .animation(.bouncy, value: isShowing)
         }
-        .padding(.horizontal)
+        .padding(.vertical, 2)
+        .onAppear(perform: {
+            accentColor = (theme == .dark ? .black : .white)
+        })
+        .onTapGesture {
+            isTyping = true
+        }
     }
-}
-
-#Preview {
-    CustomTextField()
 }
