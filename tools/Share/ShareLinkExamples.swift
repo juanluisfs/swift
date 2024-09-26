94475fd67623202e9e721382b012570489d062e5
Juan Luis Flores <juluflosa01.demos@gmail.com>
2024-09-26 17:54:27 -0600

    Update ShareLinkExamples.swift

--- tools/Share/ShareLinkExamples.swift
+++ tools/Share/ShareLinkExamples.swift
@@ -1,6 +1,14 @@
-// Simple - Sharing a URL
+// Simple: Sharing a URL with the default ShareLink label   -  Simple: Compartir un URL con un label por defecto
 ShareLink (
   item: URL(string: "https://www.google.com")!,
   subject: Text("Google Me!"),
   message: Text("Google Me Every day!")
 )
+
+// Customized - Sharing a URL with a custom SharLink label  -  Personalizado: Compartir un URL con un label personalizado
+ShareLink(item: URL(string: "https://www.google.com")!) {
+  VStack(spacing: 10) {
+    Text("Share Me!")
+    Image(systemName: "link")
+  }
+}
