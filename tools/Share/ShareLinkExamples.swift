2fe62d8aa2de7984485ab29a273f2c50bfa6e706
Juan Luis Flores <juluflosa01.demos@gmail.com>
2024-09-26 18:00:38 -0600

    Update ShareLinkExamples.swift

--- tools/Share/ShareLinkExamples.swift
+++ tools/Share/ShareLinkExamples.swift
@@ -5,8 +5,22 @@ ShareLink (
   message: Text("Google Me Every day!")
 )
 
-// Customized - Sharing a URL with a custom SharLink label  -  Personalizado: Compartir un URL con un label personalizado
+// Customized: Sharing a URL with a custom SharLink label   -  Personalizado: Compartir un URL con un label personalizado
 ShareLink(item: URL(string: "https://www.google.com")!) {
+  VStack(spacing: 10) {
+    Text("Share Me!")
+    Image(systemName: "link")
+  }
+}
+
+// Advanced: Sharing a URL with a custom label and Preview  -  Avanzado: Compartir un URL con Etiqueta y Previsualización personalizadas
+let image = Image("apple.logo")
+ShareLink(
+  item: URL(string: "https://www.google.com")!,
+  subject: Text("Google Me!"),
+  message: Text("Google Me Every day!"),
+  preview: SharePreview("Google Juan Luis", image: image)
+) {
   VStack(spacing: 10) {
     Text("Share Me!")
     Image(systemName: "link")
