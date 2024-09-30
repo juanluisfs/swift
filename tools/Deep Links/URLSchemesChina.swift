a1f471f43fda5225d0774064ab9f756614f1cd4e
Juan Luis Flores <juluflosa01.demos@gmail.com>
2024-09-29 22:11:41 -0600

    Create URLSchemesChina.swift

---
+++ tools/Deep Links/URLSchemesChina.swift
@@ -0,0 +1,19 @@
+WeChat: weixin://
+Alipay: alipay://
+Douyin (TikTok China): douyin://
+Taobao: taobao://
+JD (JingDong): jd://
+Didi Chuxing: didi://
+Meituan: meituan://
+QQ: mqq://
+Weibo: sinaweibo://
+Baidu Maps: baidumap://
+Youku: youku://
+Bilibili: bilibili://
+Xiaohongshu (Little Red Book): xiaohongshu://
+Toutiao: snssdk1128://
+Kuaishou: kwai://
+Zhihu: zhihu://
+58.com: wuba://
+Ele.me: eleme://
+Pinduoduo: pinduoduo://
