2b04b6d66e44bdc2510db5ce66b8c8421f89e6d7
Juan Luis Flores <juluflosa01.demos@gmail.com>
2024-09-29 20:52:04 -0600

    Create URLSchemes.swift

---
+++ tools/Deep Links/URLSchemes.swift
@@ -0,0 +1,32 @@
+Facebook:    fb://
+Twitter:     twitter://
+Instagram:   instagram://
+Snapchat:    snapchat://
+LinkedIn:    linkedin://
+WhatsApp:    whatsapp://
+Telegram:    tg://
+Slack:       slack://
+Amazon:      amazon://
+eBay:        ebay://
+Etsy:        etsy://
+Airbnb:      airbnb://
+Uber:        uber://
+Lyft:        lyft://
+Spotify:     spotify://
+Apple Music: music://
+YouTube:     youtube://
+Google Maps: comgooglemaps://
+Chrome:      googlechrome://
+Calendar:    calshow://
+Pinterest:   pinterest://
+TikTok:      tiktok://
+Reddit:      reddit://
+Gmail:       googlegmail://
+Messenger:   fb-messenger://
+Venmo:       venmo://
+PayPal:      paypal://
+Waze:        waze://
+Yelp:        yelp://
+Mail:        mailto://
+Phone:       tel://
+FaceTime:    facetime://
