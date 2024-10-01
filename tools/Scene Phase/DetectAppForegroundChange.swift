9e5d590cef63afde2dac5de53dd37448d07c4ad1
Juan Luis Flores <juluflosa01.demos@gmail.com>
2024-10-01 01:12:12 -0600

    Create DetectAppForegroundChange.swift

---
+++ tools/Scene Phase/DetectAppForegroundChange.swift
@@ -0,0 +1,3 @@
+.onReceive(NotificationCenter.default.publisher(for: UIScene.willEnterForegroundNotification)) { _ in 
+  // Here goes the code that you want to run  -  Aquí va el código que deseas ejecutar
+}
