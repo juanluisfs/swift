0d908827247a3c7a91852287544c7def15059013
Juan Luis Flores <juluflosa01.demos@gmail.com>
2024-10-16 20:45:19 -0600

    Update ToggleFlashlight.swift

--- tools/Flashlight/ToggleFlashlight.swift
+++ tools/Flashlight/ToggleFlashlight.swift
@@ -1,1 +1,20 @@
-
+func toggleFlash() {
+    let device = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeVideo)
+    if (device.hasTorch) {
+        do {
+            try device.lockForConfiguration()
+            if (device.torchMode == AVCaptureTorchMode.On) {
+                device.torchMode = AVCaptureTorchMode.Off
+            } else {
+                do {
+                    try device.setTorchModeOnWithLevel(1.0)
+                } catch {
+                    print(error)
+                }
+            }
+            device.unlockForConfiguration()
+        } catch {
+            print(error)
+        }
+    }
+}
