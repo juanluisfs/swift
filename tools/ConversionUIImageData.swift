7e97b809e182603bf41ee56468fe24ad68a7050f
Juan Luis Flores <juluflosa01.demos@gmail.com>
2024-10-19 20:26:38 -0600

    Update ConversionUIImageData.swift

--- tools/ConversionUIImageData.swift
+++ tools/ConversionUIImageData.swift
@@ -4,3 +4,4 @@ let image = UIImage(named: "photo")                  // Assets photo to UIImage
 let data = image?.pngData()                          // UIImage to PNG Data
 let data = image?.jpegData(compressionQuality: 1)    // UIImage to JPEG Data - Quality from 0 to 1
 let uiImage: UIImage = UIImage(data: imageData)      // Data to UIImage
+irjje
