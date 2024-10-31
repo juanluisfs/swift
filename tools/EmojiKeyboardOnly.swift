58502265972951c1e4923da08ae39de40ecc2c20
Juan Luis Flores <juluflosa01.demos@gmail.com>
2024-10-31 17:12:50 -0600

    Delete tools/EmojiKeyboardOnly.swift

--- tools/EmojiKeyboardOnly.swift
+++
@@ -1,6 +0,0 @@
-@FocusState private var isInputFocused: Bool
-
-TextField("Enter only emojis", text: $userInput)
-  .focused($isInputFocused)
-  .keyboardType(.init(rawValue: 124) ?? .default)
-  .lineLimit(1)
