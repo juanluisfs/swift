810be6bf4191be6d5c0016b0f5648678a0be7b81
Juan Luis Flores <juluflosa01.demos@gmail.com>
2024-10-31 17:09:49 -0600

    Delete tools/ConversionUIImageData.swift

--- tools/ConversionUIImageData.swift
+++
@@ -1,6 +0,0 @@
-// Swift UIImage to Data, Data to UIImage
-
-let image = UIImage(named: "photo")                  // Assets photo to UIImage
-let data = image?.pngData()                          // UIImage to PNG Data
-let data = image?.jpegData(compressionQuality: 1)    // UIImage to JPEG Data - Quality from 0 to 1
-let uiImage: UIImage = UIImage(data: imageData)      // Data to UIImage
