f5175b93c421983d8fc8264bb5c5e61acf0c219a
Juan Luis Flores <juluflosa01.demos@gmail.com>
2024-11-02 14:10:11 -0600

    Delete tools/CapitalizeFirstLetterOfString.swift

--- tools/CapitalizeFirstLetterOfString.swift
+++
@@ -1,9 +0,0 @@
-extension String {
-    func capitalizingFirstLetter() -> String {
-        return prefix(1).capitalized + dropFirst()
-    }
-
-    mutating func capitalizeFirstLetter() {
-        self = self.capitalizingFirstLetter()
-    }
-}
