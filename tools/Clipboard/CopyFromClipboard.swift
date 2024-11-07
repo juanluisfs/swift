db5225405bac9bbcb2fb9b5168e8f96577c16ed1
Juan Luis Flores <juluflosa01.demos@gmail.com>
2024-11-06 21:18:26 -0600

    Update CopyFromClipboard.swift

--- tools/Clipboard/CopyFromClipboard.swift
+++ tools/Clipboard/CopyFromClipboard.swift
@@ -1,3 +1,3 @@
-if let string = pasteboard.string { // Text found and placed in the "string" constant
-    // Add logic to handle the copied text 
+if let string = pasteboard.string { // Text found and placed in the "string" constant - Texto encontrado y guardado in la constante "string"
+    // Add logic to handle the copied text - Agregar lógica para el manejo del texto copiado
 }
