f4ed0c47b3c8873eaf8b4b69bbdbdc1fb1b37fa1
Juan Luis Flores <juluflosa01.demos@gmail.com>
2024-11-13 18:49:11 -0600

    Update DisableTextField.swift

--- tools/TextField/DisableTextField.swift
+++ tools/TextField/DisableTextField.swift
@@ -1,18 +1,18 @@
 @State var isToogleOn = false
-    @State var textFieldIsDisabled = false
-    @State var textFieldValue = ""
+@State var textFieldIsDisabled = false
+@State var textFieldValue = ""
     
-    @FocusState private var focusField: Field?
+@FocusState private var focusField: Field?
     
-    var body: some View {
-        HStack {
-            TextField("Placeholde", text: $textFieldValue)
-                .focused($focusField, equals: .textField1)
-                .disabled(textFieldIsDisabled)
-            Toggle("Activate textField", isOn: $isToogleOn)
-                .onChange(of: isToogleOn) { newValue in
-                    focusField = nil
-                    textFieldIsDisabled = !newValue
-                }
-        }
+var body: some View {
+    HStack {
+        TextField("Placeholde", text: $textFieldValue)
+            .focused($focusField, equals: .textField1)
+            .disabled(textFieldIsDisabled)
+        Toggle("Activate textField", isOn: $isToogleOn)
+            .onChange(of: isToogleOn) { newValue in
+                focusField = nil
+                textFieldIsDisabled = !newValue
+            }
     }
+}
