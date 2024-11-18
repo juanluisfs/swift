c4a2d93caa367c0e47081ff867777d48e63e4f35
Juan Luis Flores <juluflosa01.demos@gmail.com>
2024-11-18 15:23:40 -0600

    Update MixColors.swift

--- tools/Color/MixColors.swift
+++ tools/Color/MixColors.swift
@@ -1,1 +1,3 @@
+// Color red is the main color and it's going to be mixed with purple in a 0.8 (max 1.0)
+// El color rojo es el color principal que será mezclado con morado en un 0.8 (máx 1.0)
 Color.red.mix(with: .purple, by: 0.8)
