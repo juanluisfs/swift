f0848672adf6542ae8703b6a89d0cd227d6883ae
Juan Luis Flores <juluflosa01.demos@gmail.com>
2024-11-19 20:28:54 -0600

    Create FilterElements.swift

---
+++ tools/Search/FilterElements.swift
@@ -0,0 +1,21 @@
+struct ContentView: View {
+    @State private var searchText = ""
+    let allNames = ["Subh", "Vina", "Melvin", "Sefanie"]
+    var body: some View {
+        NavigationView {
+            List(filteredNames, id: \.self) { name in
+                Text(name)
+            }
+            .searchable(text: $searchText, prompt: "Look for something")
+            .navigationTitle("Searching")
+        }
+    }
+
+    var filteredNames: [String] {
+        if searchText.isEmpty {
+            return allNames
+        } else {
+            return allNames.filter { $0.localizedCaseInsensitiveContains(searchText) }
+        }
+    }
+}
