3ce0326a5bff23d99b7d26e69eb711cd726d5e81
Juan Luis Flores <juluflosa01.demos@gmail.com>
2024-12-09 22:16:42 -0600

    Update SetStepper.swift

--- tools/Stepper/SetStepper.swift
+++ tools/Stepper/SetStepper.swift
@@ -10,3 +10,20 @@ struct ContentView: View {
         }
     }
 }
+
+struct ContentView: View {
+    
+    @State var iPhoneCounter: Int = 1
+    
+    var body: some View {
+        Form {
+            Stepper("iPhone: \(iPhoneCounter)") {
+                iPhoneCounter += 1
+                print("[Incrementar] Número \(iPhoneCounter)")
+            } onDecrement: {
+                iPhoneCounter -= 1
+                print("[Decrementar] Número \(iPhoneCounter)")
+            }
+        }
+    }
+}
