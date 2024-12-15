c3871777f6b83dda20f164a81db7bac8075fc5b2
Juan Luis Flores <juluflosa01.demos@gmail.com>
2024-12-14 19:06:18 -0600

    Create PhotoPickerItemToData.swift

---
+++ tools/Photo Picker/PhotoPickerItemToData.swift
@@ -0,0 +1,8 @@
+func loadImage() {
+    Task {
+        guard let imageData = try await selectedItem?.loadTransferable(type: Data.self) else { return }
+        guard let inputImage = UIImage(data: imageData) else { return }
+
+        // more code to come
+    }
+}
