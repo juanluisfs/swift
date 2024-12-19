5985fb5080133a02c2bebef6166d331afd577252
Juan Luis Flores <juluflosa01.demos@gmail.com>
2024-12-18 19:49:07 -0600

    Create SlidingText.swift

---
+++ tools/Text/SlidingText.swift
@@ -0,0 +1,24 @@
+import SwiftUI
+
+struct DemoSlideText: View {
+    let text = "Lorem ipsum dolor sit amet, consetetur sadipsing elitr, sed diam nonumy eirmod tempor"
+    
+    @State private var go = false
+    
+    var body: some View {
+        VStack {
+            Text(text)
+        }
+        .fixedSize()
+        .frame(width: 50, alignment: go ? .trailing:.leading)
+        .clipped()
+        .onAppear {
+            self.go.toggle()
+        }
+        .animation(.linear(duration: 20).repeatForever(autoreverses: true), value: go)
+    }
+}
+
+#Preview {
+    DemoSlideText()
+}
