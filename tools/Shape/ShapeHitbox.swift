57ce3ae4a1740d52af875d2d5b58b955e6f4f9cd
Juan Luis Flores <juluflosa01.demos@gmail.com>
2024-12-24 10:37:03 -0600

    Update ShapeHitbox.swift

--- tools/Shape/ShapeHitbox.swift
+++ tools/Shape/ShapeHitbox.swift
@@ -1,9 +1,13 @@
+// Hitbox fitting the shape
+// Zona de impacto que se ajusta a la figura
 Circle()
   .frame(width: 400)
   .onTapGesture(
     print("Tapped")
   )
 
+// Hitbox fitting the shape described on contentShape
+// Zona de impacto que se ajusta a la figura descrita en contentShape
 Circle()
   .frame(width: 400)
   .contentShape(.rect)
