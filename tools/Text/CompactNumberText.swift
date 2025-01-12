48e35e23b9a9f76cd0c7b8e691fc08039a450f5d
Juan Luis Flores <juluflosa01.demos@gmail.com>
2025-01-11 19:32:32 -0600

    Update CompactNumberText.swift

--- tools/Text/CompactNumberText.swift
+++ tools/Text/CompactNumberText.swift
@@ -1,1 +1,5 @@
-Text(1000, format: .number.notation(.compactName))  // Shows 1K
+Text(1000, format: .number.notation(.compactName))     // Shows 1K
+
+Text(10000, format: .number.notation(.compactName))    // Shows 10K
+
+Text(1000000, format: .number.notation(.compactName))  // Shows 1M
