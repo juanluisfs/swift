5a2c32f9431e2f1296e8fc0e4d9075bcf9d88a8b
Juan Luis Flores <juluflosa01.demos@gmail.com>
2025-01-27 20:17:06 -0600

    Create TryErrorHandling.swift

---
+++ tools/Error Handling/TryErrorHandling.swift
@@ -0,0 +1,11 @@
+func updateItem(_ card: CardData) {
+        // Edite the item Data
+        
+        
+        do {
+            try context.save()
+        } catch {
+            print("The file could not be loaded")
+        }
+    }
+    
