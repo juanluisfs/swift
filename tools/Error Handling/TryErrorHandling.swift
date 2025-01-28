cb913042eebc62c22e7f1dbd61dbf9ef83791b92
Juan Luis Flores <juluflosa01.demos@gmail.com>
2025-01-27 20:22:00 -0600

    Update TryErrorHandling.swift

--- tools/Error Handling/TryErrorHandling.swift
+++ tools/Error Handling/TryErrorHandling.swift
@@ -1,8 +1,10 @@
 func updatingSomething(_ something: somethingData){
         do {
             try context.save()
+                // Code to execute if there is no error
         } catch {
             print("The file could not be loaded")
+                // Code to execute if there is an error
         }
     }
     
