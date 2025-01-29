04181daa7bd0938e24ffaf0990c135644da2629f
Juan Luis Flores <juluflosa01.demos@gmail.com>
2025-01-28 20:11:12 -0600

    Update ChangeOrientationProgramatically.swift

--- tools/Orientation/ChangeOrientationProgramatically.swift
+++ tools/Orientation/ChangeOrientationProgramatically.swift
@@ -1,1 +1,13 @@
+import SwiftUI
 
+@main
+struct OrientationControlApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) private var appDelegate
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
