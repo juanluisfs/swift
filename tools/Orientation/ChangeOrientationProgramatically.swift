5f54639d04c1386ec5f4b4a6849f7f819cfba06d
Juan Luis Flores <juluflosa01.demos@gmail.com>
2025-01-28 20:11:25 -0600

    Update ChangeOrientationProgramatically.swift

--- tools/Orientation/ChangeOrientationProgramatically.swift
+++ tools/Orientation/ChangeOrientationProgramatically.swift
@@ -11,3 +11,16 @@ struct OrientationControlApp: App {
         }
     }
 }
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    // If you want to begin with portrait mode, set this to portrait
+    static var orientation: UIInterfaceOrientationMask = .all
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        return true
+    }
+    
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        return Self.orientation
+    }
+}
