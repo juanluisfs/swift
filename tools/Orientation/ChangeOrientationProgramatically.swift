7793495b5e0aac71ecbbf47cf1e4110fec1d77a9
Juan Luis Flores <juluflosa01.demos@gmail.com>
2025-01-28 20:12:08 -0600

    Update ChangeOrientationProgramatically.swift

--- tools/Orientation/ChangeOrientationProgramatically.swift
+++ tools/Orientation/ChangeOrientationProgramatically.swift
@@ -1,5 +1,6 @@
 import SwiftUI
 
+// Main App Code
 @main
 struct OrientationControlApp: App {
     
@@ -24,3 +25,55 @@ class AppDelegate: NSObject, UIApplicationDelegate {
         return Self.orientation
     }
 }
+
+// Content View Code
+enum Orientation: String, CaseIterable {
+    case all = "All"
+    case portrait = "Portrait"
+    case landscapeLeft = "Left"
+    case landscapeRight = "Right"
+    
+    var mask: UIInterfaceOrientationMask {
+        switch self {
+        case .all: return .all
+        case .portrait: return .portrait
+        case .landscapeLeft: return .landscapeLeft
+        case .landscapeRight: return .landscapeRight
+        }
+    }
+}
+
+struct ContentView: View {
+    
+    @State private var orientation: Orientation = .portrait
+    
+    var body: some View {
+        NavigationStack {
+            List {
+                Section("Orientation") {
+                    Picker("", selection: $orientation) {
+                        ForEach(Orientation.allCases, id: \.rawValue) { orientation in
+                            Text(orientation.rawValue)
+                                .tag(orientation)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                    .onChange(of: orientation, initial: true) { oldValue, newValue in
+                        modifyOrientation(newValue.mask)
+                    }
+                }
+            }
+        }
+    }
+}
+
+extension View {
+    func modifyOrientation(_ mask: UIInterfaceOrientationMask) {
+        if let windowScene = (UIApplication.shared.connectedScenes.first as? UIWindowScene) {
+            AppDelegate.orientation = mask
+            windowScene.requestGeometryUpdate(.iOS(interfaceOrientations: mask))
+            windowScene.keyWindow?.rootViewController?.setNeedsUpdateOfSupportedInterfaceOrientations()
+        }
+    }
+}
+
