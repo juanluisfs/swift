9c27b48dfda5c3ba6e95c09ac1f6db0214f97ea6
Juan Luis Flores <juluflosa01.demos@gmail.com>
2025-02-02 17:33:31 -0600

    Update AddButtonToKeyboard.swift

--- tools/Keyboard/AddButtonToKeyboard.swift
+++ tools/Keyboard/AddButtonToKeyboard.swift
@@ -1,14 +1,17 @@
 .toolbar(content: {
+  // FocusedState neccesary if the button is shown twice
   if isTyping {
     ToolbarItem(placement: .keyboard) {
       Button {
         if isTyping {
-          
+          // Action to execute when tapped
         } label: {
+          // Label shown on the keyboard
           Text("Done")
             .fontWeight(.bold)
             .tint(.blue)
         }
+        // Alignment of the button on the keyboard
         .frame(maxWidth: .infinity, alignment: .trailing)
       }
     }
