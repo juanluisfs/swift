df0c69b9389446ac4ff104dfa4d2900c0b159ada
Juan Luis Flores <juluflosa01.demos@gmail.com>
2025-02-04 20:41:48 -0600

    Update ListHeadeAndFooter.swift

--- tools/List/ListHeadeAndFooter.swift
+++ tools/List/ListHeadeAndFooter.swift
@@ -1,9 +1,10 @@
 List {
+    // Section with Header
     Section(header: Text("Other tasks")) {
-                TaskRow()
-                TaskRow()
-                TaskRow()
-            }
+        TaskRow()
+        TaskRow()
+        TaskRow()
+    }
     
     Section(header: Text("Other tasks"), footer: Text("End")) {
         Text("Row 1")
