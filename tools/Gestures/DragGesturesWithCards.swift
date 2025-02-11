a443199e08e9f01586a92e1838088aaee4a1e86c
Juan Luis Flores <juluflosa01.demos@gmail.com>
2025-02-10 19:11:33 -0600

    Create DragGesturesWithCards.swift

---
+++ tools/Gestures/DragGesturesWithCards.swift
@@ -0,0 +1,78 @@
+//
+//  DragGestureView.swift
+//  kard
+//
+//  Created by Juan Luis Flores on 10/02/25.
+//
+
+import SwiftUI
+
+struct DragGestureView: View {
+    
+    @State var viewSate = CGSize.zero
+    
+    var body: some View {
+        ZStack {
+            CardBack()
+                .animation(.spring, value: viewSate)
+                .offset(y: viewSate.height > 100 ? -100 : (viewSate.height < -100 ? 100 : 0))
+                .offset(y: -50)
+                .onTapGesture {
+                    self.viewSate = .zero
+                }
+            
+            Card()
+                .offset(y: viewSate.height)
+                .animation(.spring, value: viewSate.height)
+                .gesture(
+                    DragGesture()
+                        .onChanged({ value in
+                            self.viewSate = value.translation
+                        })
+                        .onEnded({ value in
+                            if self.viewSate.height > 200 {
+                                self.viewSate = CGSize(width: 0, height: 600)
+                            } else {
+                                self.viewSate = .zero
+                            }
+                        })
+                )
+        }
+        .padding(.top, 100.0)
+        .background(Color.black)
+    }
+}
+
+struct CardBack: View {
+    var body: some View {
+        VStack {
+            Text("Build an app with SwiftUI")
+                .frame(maxWidth: .infinity)
+            
+            Spacer()
+        }
+        .background {
+            RoundedRectangle(cornerRadius: 20)
+                .foregroundStyle(.blue)
+        }
+    }
+}
+
+struct Card: View {
+    var body: some View {
+        VStack {
+            Text("Build an app with SwiftUI")
+                .frame(maxWidth: .infinity)
+            
+            Spacer()
+        }
+        .background {
+            RoundedRectangle(cornerRadius: 20)
+                .foregroundStyle(.red)
+        }
+    }
+}
+
+#Preview {
+    DragGestureView()
+}
