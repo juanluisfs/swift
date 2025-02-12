4d1e84dd73379550e8558acad7e7f2ff0832b7be
Juan Luis Flores <juluflosa01.demos@gmail.com>
2025-02-11 21:13:44 -0600

    Create LockWidgetWithDeepLink.swift

---
+++ tools/Widgets/Lock Widgets/LockWidgetWithDeepLink.swift
@@ -0,0 +1,73 @@
+import WidgetKit
+import SwiftUI
+
+struct Provider: TimelineProvider {
+    func placeholder(in context: Context) -> SimpleEntry {
+        SimpleEntry(date: Date())
+    }
+
+    func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
+        let entry = SimpleEntry(date: Date())
+        completion(entry)
+    }
+
+    func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
+        var entries: [SimpleEntry] = []
+
+        let currentDate = Date()
+        for hourOffset in 0 ..< 5 {
+            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
+            let entry = SimpleEntry(date: entryDate)
+            entries.append(entry)
+        }
+
+        let timeline = Timeline(entries: entries, policy: .atEnd)
+        completion(timeline)
+    }
+}
+
+struct SimpleEntry: TimelineEntry {
+    let date: Date
+}
+
+struct LockEntryView : View {
+    var entry: Provider.Entry
+    @Environment(\.widgetFamily) private var family
+
+    var body: some View {
+        ZStack {
+            switch family {
+            case .accessoryCircular:
+                VStack {
+                    Image("Placeholder")
+                        .resizable()
+                        .padding(12)
+                }
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+            default:
+                Text("No Widget Available")
+            }
+        }
+        .widgetURL(URL(string: "kard://open-card")) // Necessary when .accesoryCircular, in othe
+    }
+}
+
+struct KardLockWidget: Widget {
+    let kind: String = "LockWidget"
+
+    var body: some WidgetConfiguration {
+        StaticConfiguration(kind: kind, provider: Provider()) { entry in
+            if #available(iOS 17.0, *) {
+                LockEntryView(entry: entry)
+                    .containerBackground(.fill, for: .widget)
+            } else {
+                LockEntryView(entry: entry)
+                    .padding()
+                    .background()
+            }
+        }
+        .supportedFamilies([.accessoryCircular])
+        .configurationDisplayName("Kard Lock Widget")
+        .description("Open your Quick Kard from Lock Screen")
+    }
+}
