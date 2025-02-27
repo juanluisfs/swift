eab71fc97c6c4370dc4f918cbead1f172ac15549
Juan Luis Flores <juluflosa01.demos@gmail.com>
2025-02-26 20:15:44 -0600

    Create MenuInsideMenu.swift

---
+++ tools/Menu/MenuInsideMenu.swift
@@ -0,0 +1,11 @@
+Menu {
+  Menu {
+    Button("Share with Friends")
+
+    Button("Save on Device")
+  } label: {
+    Label("Share", systemImage: "square.and.arrow.up)
+  }
+} label: {
+  Label("Share", systemImage: "square.and.arrow.up)
+}
