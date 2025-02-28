eb93f04a8a50ebc1edfe6a9e650a2c0f112abe4a
Juan Luis Flores <juluflosa01.demos@gmail.com>
2025-02-27 20:03:02 -0600

    Create DisplayDeviceInformation.swift

---
+++ tools/Device Information/DisplayDeviceInformation.swift
@@ -0,0 +1,11 @@
+// System Name
+Text(UIDevice.current.systemName)
+
+// System Version
+Text(UIDevice.current.systemVersion)
+
+// Device Model
+Text(UIDevice.current.model)
+
+// Device Name
+Text(UIDevice.current.name)
