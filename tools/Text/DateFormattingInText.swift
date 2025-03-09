c5bf6f09b15d20351d152432e71a8cb68bb76422
Juan Luis Flores <juluflosa01.demos@gmail.com>
2025-03-09 10:53:18 -0600

    Update DateFormattingInText.swift

--- tools/Text/DateFormattingInText.swift
+++ tools/Text/DateFormattingInText.swift
@@ -1,19 +1,29 @@
+// Day Number
 Text(Date().formatted(.dateTime.day()))
 
+// Week Number
 Text(Date().formatted(.dateTime.week()))
 
+// Weekday Short
 Text(Date().formatted(.dateTime.weekday()))
 
+// Weekday Complete
 Text(Date().formatted(.dateTime.weekday(.wide)))
 
+// Month Short
 Text(Date().formatted(.dateTime.month()))
 
+// Month Complete
 Text(Date().formatted(.dateTime.month(.wide)))
 
+// Year (4-Digit)
 Text(Date().formatted(.dateTime.year()))
 
+// Day Number with Short Weekday
 Text(Date().formatted(.dateTime.day().weekday()))
 
+// Day Number, Short Weekday and Short Month
 Text(Date().formatted(.dateTime.day().weekday().month()))
 
+// Day Number, Short Weekday, Short Month adn Year (4-Digit)
 Text(Date().formatted(.dateTime.day().weekday().month().year()))
