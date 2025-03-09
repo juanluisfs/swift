6829c857db69f098985e943195ffbf60d82602b2
Juan Luis Flores <juluflosa01.demos@gmail.com>
2025-03-09 10:50:42 -0600

    Create DateFormattingInText.swift

---
+++ tools/Text/DateFormattingInText.swift
@@ -0,0 +1,19 @@
+Text(Date().formatted(.dateTime.day()))
+            
+            Text(Date().formatted(.dateTime.week()))
+            
+            Text(Date().formatted(.dateTime.weekday()))
+            
+            Text(Date().formatted(.dateTime.weekday(.wide)))
+            
+            Text(Date().formatted(.dateTime.month()))
+            
+            Text(Date().formatted(.dateTime.month(.wide)))
+            
+            Text(Date().formatted(.dateTime.year()))
+            
+            Text(Date().formatted(.dateTime.day().weekday()))
+            
+            Text(Date().formatted(.dateTime.day().weekday().month()))
+            
+            Text(Date().formatted(.dateTime.day().weekday().month().year()))
