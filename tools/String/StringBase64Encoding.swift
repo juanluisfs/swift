291c021f754dcd764679c9448fce3d84b972ef5a
Juan Luis Flores <juluflosa01.demos@gmail.com>
2025-04-10 20:00:00 -0600

    Create StringBase64Encoding.swift

---
+++ tools/String/StringBase64Encoding.swift
@@ -0,0 +1,13 @@
+extension String {
+    func fromBase64() -> String? {
+        guard let data = Data(base64Encoded: self) else {
+            return nil
+        }
+
+        return String(data: data, encoding: .utf8)
+    }
+
+    func toBase64() -> String {
+        return Data(self.utf8).base64EncodedString()
+    }
+}
