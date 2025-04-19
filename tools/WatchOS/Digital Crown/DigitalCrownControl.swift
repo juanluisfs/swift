22e386c69a0b2dd4fdd302f7a312a8046435033a
Juan Luis Flores <juluflosa01.demos@gmail.com>
2025-04-18 23:04:25 -0600

    Update DigitalCrownControl.swift

--- tools/WatchOS/Digital Crown/DigitalCrownControl.swift
+++ tools/WatchOS/Digital Crown/DigitalCrownControl.swift
@@ -1,1 +1,10 @@
-.digitalCrownRotation($scrollAmount, from: 1, through: 5, by: 0.1, sensitivity: .low, isContinuous: true, isHapticFeedbackEnabled: true)
+struct ContentView: View {
+    @State var scrollAmount = 0.0
+
+    var body: some View {
+        Text("Scroll: \(scrollAmount)")
+            .focusable(true)
+            .digitalCrownRotation($scrollAmount)
+      .digitalCrownRotation($scrollAmount, from: 1, through: 5, by: 0.1, sensitivity: .low, isContinuous: true, isHapticFeedbackEnabled: true)
+    }
+}
