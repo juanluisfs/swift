6989bae2bde716d81bd4fe59c88a64ac7de63929
Juan Luis Flores <juluflosa01.demos@gmail.com>
2025-05-21 23:01:39 -0600

    Create ViewBasedOnDeviceType.swift

---
+++ tools/Views/ViewBasedOnDeviceType.swift
@@ -0,0 +1,12 @@
+@main
+struct AppMain: App {
+  var body: some Scene {
+    WindowGroup {
+      if UIDevice.isiPhone {
+        iPhoneView()
+      } else {
+        iPadView()
+      }
+    }
+  }
+}
