c7701ea09e99612431fc0699143652852fb0b7ad
Juan Luis Flores <juluflosa01.demos@gmail.com>
2025-05-29 23:13:21 -0600

    Update ListBackgroundClearWatchOS.swift

--- tools/List/ListBackgroundClearWatchOS.swift
+++ tools/List/ListBackgroundClearWatchOS.swift
@@ -1,4 +1,6 @@
 // Change the insets of the element in the list
 .listRowInsets(EdgeInsets.init(top: 4, leading: 0, bottom: 0, trailing: 0))
+
+// Add a view as the background of the element
 .listRowBackground(RoundedRectangle(cornerRadius: 20).foregroundStyle(.white))
 .listRowPlatterColor(.white)
