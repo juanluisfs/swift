08b81b2351e41ca9d24973154ca9a7f23b54a519
Juan Luis Flores <juluflosa01.demos@gmail.com>
2025-05-29 23:12:16 -0600

    Create ListBackgroundClearWatchOS.swift

---
+++ tools/List/ListBackgroundClearWatchOS.swift
@@ -0,0 +1,3 @@
+.listRowInsets(EdgeInsets.init(top: 4, leading: 0, bottom: 0, trailing: 0))
+.listRowBackground(RoundedRectangle(cornerRadius: 20).foregroundStyle(.white))
+.listRowPlatterColor(.white)
