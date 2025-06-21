f41039d997155b8445aa3d62a49b934155fccdaa
Juan Luis Flores <juluflosa01.demos@gmail.com>
2025-06-21 12:38:26 -0600

    Create CustomProgressView.swift

---
+++ tools/Progress View/CustomProgressView.swift
@@ -0,0 +1,34 @@
+struct GaugeProgressStyle: ProgressViewStyle {
+    var strokeColor = Color.blue
+    var strokeWidth = 25.0
+
+    func makeBody(configuration: Configuration) -> some View {
+        let fractionCompleted = configuration.fractionCompleted ?? 0
+
+        return ZStack {
+            Circle()
+                .trim(from: 0, to: fractionCompleted)
+                .stroke(strokeColor, style: StrokeStyle(lineWidth: strokeWidth, lineCap: .round))
+                .rotationEffect(.degrees(-90))
+        }
+    }
+}
+
+// A view letting you adjust the progress with tap gestures
+struct ContentView: View {
+    @State private var progress = 0.2
+
+    var body: some View {
+        ProgressView(value: progress, total: 1.0)
+            .progressViewStyle(GaugeProgressStyle())
+            .frame(width: 200, height: 200)
+            .contentShape(Rectangle())
+            .onTapGesture {
+                if progress < 1.0 {
+                    withAnimation {
+                        progress += 0.2
+                    }
+                }
+            }
+    }
+}
