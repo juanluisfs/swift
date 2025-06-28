ba4b059deeca8312bcf22bb6f66d03348db4d92e
Juan Luis Flores <juluflosa01.demos@gmail.com>
2025-06-27 23:52:00 -0600

    Create DrawPDFImageFromURL.swift

---
+++ tools/PDF/DrawPDFImageFromURL.swift
@@ -0,0 +1,18 @@
+func drawPDFfromURL(url: URL) -> UIImage? {
+    guard let document = CGPDFDocument(url as CFURL) else { return nil }
+    guard let page = document.page(at: 1) else { return nil }
+
+    let pageRect = page.getBoxRect(.mediaBox)
+    let renderer = UIGraphicsImageRenderer(size: pageRect.size)
+    let img = renderer.image { ctx in
+        UIColor.white.set()
+        ctx.fill(pageRect)
+
+        ctx.cgContext.translateBy(x: 0.0, y: pageRect.size.height)
+        ctx.cgContext.scaleBy(x: 1.0, y: -1.0)
+
+        ctx.cgContext.drawPDFPage(page)
+    }
+
+    return img
+}
