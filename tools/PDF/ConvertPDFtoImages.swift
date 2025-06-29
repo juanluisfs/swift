98e1202700e078ce30be5dce2866a547babe2430
Juan Luis Flores <juluflosa01.demos@gmail.com>
2025-06-28 23:26:24 -0600

    Update ConvertPDFtoImages.swift

--- tools/PDF/ConvertPDFtoImages.swift
+++ tools/PDF/ConvertPDFtoImages.swift
@@ -1,1 +1,27 @@
-//
+func convertPDFToImages(pdfURL: URL) -> [UIImage]? {
+        guard let pdfDocument = PDFDocument(url: pdfURL) else {
+            return nil
+        }
+        
+        var images: [UIImage] = []
+        
+        for pageNum in 0..<pdfDocument.pageCount {
+            if let pdfPage = pdfDocument.page(at: pageNum) {
+                let pdfPageSize = pdfPage.bounds(for: .mediaBox)
+                let renderer = UIGraphicsImageRenderer(size: pdfPageSize.size)
+                
+                let image = renderer.image { ctx in
+                    UIColor.white.set()
+                    ctx.fill(pdfPageSize)
+                    ctx.cgContext.translateBy(x: 0.0, y: pdfPageSize.size.height)
+                    ctx.cgContext.scaleBy(x: 1.0, y: -1.0)
+                    
+                    pdfPage.draw(with: .mediaBox, to: ctx.cgContext)
+                }
+                
+                images.append(image)
+            }
+        }
+        
+        return images
+    }
