19dba980be33e44ba034230eeee52b43231fe39f
Juan Luis Flores <juluflosa01.demos@gmail.com>
2025-06-30 22:30:53 -0600

    Update SwiftDataMigration.swift

--- tools/SwiftData/SwiftDataMigration.swift
+++ tools/SwiftData/SwiftDataMigration.swift
@@ -25,3 +25,30 @@ extension DataSchemaV1 {
         }
     }
 }
+
+enum DataSchemaV2: VersionedSchema {
+    static var models: [any PersistentModel.Type] {
+        [DataItem.self]
+    }
+    
+    static var versionIdentifier: Schema.Version = .init(1, 0, 1)
+}
+
+extension DataSchemaV2 {
+    @Model
+    class DataItem: Identifiable {
+        
+        var id: String
+        var name: String
+        var date: Date = Date.now
+        
+        @Attribute(.externalStorage)
+        var image: Data?
+        
+        init(name: String, date: Date) {
+            self.id = UUID().uuidString
+            self.name = name
+            self.date = date
+        }
+    }
+}
