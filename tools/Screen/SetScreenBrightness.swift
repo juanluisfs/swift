de748282cd843a9c3b5b49dda523acba5956900c
Juan Luis Flores <juluflosa01.demos@gmail.com>
2025-11-16 01:59:51 -0600

    Update SetScreenBrightness.swift

--- tools/Screen/SetScreenBrightness.swift
+++ tools/Screen/SetScreenBrightness.swift
@@ -1,1 +1,16 @@
+// Deprecated iOS26
 UIScreen.main.brightness = 1.0
+
+// Updated Version
+func setScreenBrightness(_ value: CGFloat) {
+    let scene = UIApplication.shared.connectedScenes
+        .compactMap { $0 as? UIWindowScene }
+        .first
+  
+    if let screen = scene?.screen {
+        screen.brightness = value
+    }
+}
+
+// Example
+setScreenBrightness(0.9)
